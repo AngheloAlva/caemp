eef6b5566da08c6ef6dbeaecb2052f418a878427
Anghelo Alva <ance.anghelo@gmail.com>
2026-01-22 18:37:53 -0300

    Add white logo when card is hover

--- src/components/sections/grupo/companies.tsx
+++ src/components/sections/grupo/companies.tsx
@@ -14,6 +14,7 @@ const companies = [
 	{
 		name: "CAEMP (OTEC)",
 		logo: "/images/logo/logo-o-black.png",
+		whiteLogo: "/images/logo/logo-o-white.png",
 		color: "#0066b3", // Azul
 		description:
 			"Capacitación, entrenamiento y respuesta ante emergencias. Diseñamos programas certificados por SENCE en sectores como minería, energía, telecomunicaciones y maestranzas.",
@@ -24,6 +25,7 @@ const companies = [
 	{
 		name: "CAEMP PLUS",
 		logo: "/images/logo/logo-p-black.png",
+		whiteLogo: "/images/logo/logo-p-white.png",
 		color: "#00a651", // Verde
 		description:
 			"Comercialización de equipos de protección personal y colectiva de última generación. Brindamos asesoría técnica especializada para garantizar la seguridad en cada entorno de trabajo.",
@@ -34,6 +36,7 @@ const companies = [
 	{
 		name: "CRECIMIENTO",
 		logo: "/images/logo/logo-c-black.png",
+		whiteLogo: "/images/logo/logo-c-white.png",
 		color: "#9b2789", // Púrpura
 		description:
 			"Potenciamos el liderazgo, la comunicación y la colaboración. Diseñamos programas de capacitación y talleres personalizados que fortalecen las habilidades blandas.",
@@ -74,10 +77,10 @@ export default function Companies() {
 										}
 									)}
 								>
-									<div className="absolute inset-0 flex items-end">
+									<div className="absolute inset-0 -z-10 flex items-end">
 										<div
 											className={cn(
-												"bg-primary -z-10 h-0 w-full transition-all duration-500 group-hover:h-full",
+												"bg-primary h-0 w-full transition-all duration-500 group-hover:h-full",
 												{
 													"bg-primary-green": index === 1,
 													"bg-primary-purple": index === 2,
@@ -92,7 +95,14 @@ export default function Companies() {
 											height={90}
 											alt={company.name}
 											src={company.logo}
-											className="h-22 w-auto object-contain"
+											className="h-22 w-auto object-contain group-hover:hidden"
+										/>
+										<Image
+											width={90}
+											height={90}
+											alt={company.name}
+											src={company.whiteLogo}
+											className="hidden h-22 w-auto object-contain group-hover:block"
 										/>
 										<p className="mt-2 text-sm tracking-wide text-gray-400 uppercase transition-colors duration-500 group-hover:text-gray-200">
 											{company.name}
@@ -129,24 +139,22 @@ export default function Companies() {
 										</p>
 									</div>
 
-									<div>
-										<TenantLink to={company.link}>
-											<motion.button
-												whileTap={{ scale: 0.95 }}
-												whileHover={{ scale: 1.05 }}
-												aria-label={`Ver más sobre ${company.name}`}
-												className={cn(
-													"group border-primary text-primary flex h-14 w-14 items-center justify-center rounded-full border-2 duration-100 group-hover:border-white group-hover:text-white hover:scale-105",
-													{
-														"text-primary-green border-primary-green": index === 1,
-														"text-primary-purple border-primary-purple": index === 2,
-													}
-												)}
-											>
-												<ArrowRight className="h-5 w-5 transition-colors group-hover:text-white" />
-											</motion.button>
-										</TenantLink>
-									</div>
+									<TenantLink to={company.link}>
+										<motion.button
+											whileTap={{ scale: 0.95 }}
+											whileHover={{ scale: 1.05 }}
+											aria-label={`Ver más sobre ${company.name}`}
+											className={cn(
+												"group border-primary text-primary z-50 flex h-14 w-14 cursor-pointer items-center justify-center rounded-full border-2 duration-100 group-hover:border-white group-hover:text-white hover:scale-105",
+												{
+													"text-primary-green border-primary-green": index === 1,
+													"text-primary-purple border-primary-purple": index === 2,
+												}
+											)}
+										>
+											<ArrowRight className="h-5 w-5 transition-colors group-hover:text-white" />
+										</motion.button>
+									</TenantLink>
 								</div>
 							</motion.div>
 						</StaggerItem>
