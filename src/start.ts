a79be030f99da2eea809ad0be572aa2d8d0ca12c
Anghelo Alva <ance.anghelo@gmail.com>
2026-01-22 12:27:13 -0300

    Add getRouter in start.ts

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,8 @@
 import { createStart } from "@tanstack/react-start"
 import { tenantMiddleware } from "./middleware/tenant"
+import { getRouter } from "./router"
 
 export const startInstance = createStart(() => ({
+	app: getRouter,
 	requestMiddleware: [tenantMiddleware],
 }))
