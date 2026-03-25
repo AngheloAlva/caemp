3e08311d430cadbfbeee16a89a58f8a910c9446e
Anghelo Alva <ance.anghelo@gmail.com>
2026-03-25 11:29:53 -0300

    Update phone in floating social media for otec

--- src/components/shared/floating-social-media.tsx
+++ src/components/shared/floating-social-media.tsx
@@ -19,7 +19,7 @@ const SOCIAL_CONFIG: Record<DomainType, { instagram: string; whatsapp: string }>
 	},
 	otec: {
 		instagram: "https://www.instagram.com/caemp.cl",
-		whatsapp: "56988080195",
+		whatsapp: "56944769378",
 	},
 	group: {
 		instagram: "https://www.instagram.com/caemp.cl",
